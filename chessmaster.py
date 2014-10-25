81a6a2680d196833162e9c286fb7a1da4f8d61ee
Joseph Graham <josgraha@gmail.com>
2014-10-25 18:42:28 -0400

    task05

--- chessmaster.py
+++ chessmaster.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """Lesson 08 Chess Master."""
+import sys
+import traceback
 import time
 
 
@@ -14,26 +16,40 @@ class ChessPiece(object):
        position (string): The position in chess notation.
        moves (tuple): Move history of this piece.
     """
-    prefix = ''
     cols = 'abcdefgh'
-    # noinspection PyBroadException
-    @staticmethod
-    def algebraic_to_numeric(tile):
-        if tile is None:
-            return None
-        try:
-            x = ChessPiece.cols.find(tile[0])
-            y = tile[1] - 1
-            return x, y
-        except Exception:
-            return None
-        return None
+    prefix = ''
 
-    def is_legal_move(position):
-        return True if algebraic_to_numeric(position) is not None else False
-
-    def __init__(self, position=None):
-        if not ChessPiece.is_legal_move(position):
+    def __init__(self, position):
+        if not self.is_legal_move(position):
             excep = '`{}` is not a legal start position'
             raise ValueError(excep.format(position))
         self.position = position
+        self.moves = []
+        self.prefix = ChessPiece.prefix
+
+    def algebraic_to_numeric(self, tile=None):
+        #print 'tile: {}'.format(tile)
+        if tile is None:
+            return None
+        try:
+            y = int(tile[1]) - 1
+            x = ChessPiece.cols.find(tile[0])
+            #print 'x: {}, y: {}'.format(x, y)
+            if y > 7 or y < 0 or x < 0:
+                return None
+            return x, y
+        except:
+            traceback.print_exc(file=sys.stdout)
+            return None
+        return None
+
+    def is_legal_move(self, position):
+        return True if self.algebraic_to_numeric(position) is not None else False
+
+    def move(self, position):
+        if not self.is_legal_move(position):
+            return False
+        move = ('{}{}'.format(self.prefix,self.position), '{}{}'.format(self.prefix, position), time.time())
+        self.moves.append(move)
+        self.position = position
+        return move
