842c02bedff5a5682a7c28eb03b9c44d44d3d27a
Joseph Graham <josgraha@gmail.com>
2014-10-28 00:03:15 -0400

    chessmaster minus XC so far

--- chessmaster.py
+++ chessmaster.py
@@ -27,20 +27,19 @@ class Helper(object):
 
     @staticmethod
     def numeric_position(tile=None):
-        print 'numeric_position: tile: {}'.format(tile)
+        # print 'numeric_position: tile: {}'.format(tile)
         if tile is None:
             return None
         try:
             y = int(tile[1]) - 1
             x = Helper.cols.find(tile[0])
-            print 'numeric_position: (x: {}, y: {})'.format(x, y)
+            # print 'numeric_position: (x: {}, y: {})'.format(x, y)
             if y > 7 or y < 0 or x < 0:
                 return None
             return x, y
         except:
             traceback.print_exc(file=sys.stdout)
             return None
-        return None
 
     @staticmethod
     def is_position_valid(position):
@@ -48,103 +47,152 @@ class Helper(object):
 
     @staticmethod
     def algebraic_to_numeric(tile=None):
-        print 'algebraic_to_numeric: tile: {}'.format(tile)
+        # print 'algebraic_to_numeric: tile: {}'.format(tile)
         if tile is None:
             return None
         try:
             y = int(tile[1]) - 1
             x = Helper.cols.find(tile[0])
-            print 'x: {}, y: {}'.format(x, y)
+            # print 'x: {}, y: {}'.format(x, y)
             if y > 7 or y < 0 or x < 0:
                 return None
             return x, y
         except:
             traceback.print_exc(file=sys.stdout)
             return None
-        return None
 
     @staticmethod
-    def numeric_to_algebraic(coord):
-        print 'numeric_to_algebraic: tile: {}'.format(coord)
+    def coord_to_algebraic(coord):
+        # print 'coord_to_algebraic: coord: {}'.format(coord)
         if coord is None:
             return None
+        x, y = coord
+        return Helper.numeric_to_algebraic(x, y)
+
+    @staticmethod
+    def numeric_to_algebraic(x, y):
+        # print 'numeric_to_algebraic: x: {}, y: {}'.format(x, y)
+        if x is None or y is None:
+            return None
         try:
-            x, y = coord
-            if y > 7 or y < 0 or x < 0:
+            if y > 7 or y < 0 or x < 0 or x > 7:
                 return None
             row = int(y) + 1
-            col = Helper.cols.index(x)
+            col = Helper.cols[x]
             position = '{}{}'.format(col, row)
-            print 'position: {}'.format(position)
+            # print 'position: {}'.format(position)
             return position
         except:
             traceback.print_exc(file=sys.stdout)
             return None
-        return None
 
     @staticmethod
-    def get_next_moves(position, move_type, move_dir, distance=None):
+    def get_moves(position, move_type, move_dir, distance=None):
         if not Helper.is_position_valid(position):
             return None
         x, y = Helper.algebraic_to_numeric(position)
         moves = []
         distance = 7 if distance is None else distance
         while distance > 0:
-            next_pos = None
-            if move_type == Helper.MOVE_TYPE.DIAGONAL:
-                if move_dir == Helper.DIAGONAL_MOVES.NW:
-                    x, y = (x-1, y-1)
-                    next_pos = Helper.numeric_to_algebraic((x, y))
-                elif move_dir == Helper.DIAGONAL_MOVES.NE:
-                    x, y = (x+1, y-1)
-                    next_pos = Helper.numeric_to_algebraic((x, y))
-                elif move_dir == Helper.DIAGONAL_MOVES.SW:
-                    x, y = (x+1, y+1)
-                    next_pos = Helper.numeric_to_algebraic((x, y))
-                elif move_dir == Helper.DIAGONAL_MOVES.SE:
-                    x, y = (x-1, y+1)
-                    next_pos = Helper.numeric_to_algebraic((x, y))
-            elif move_type == Helper.MOVE_TYPE.LINEAR:
-                if move_dir == Helper.LINEAR_MOVES.UP:
-                    x, y = (x, y-1)
-                    next_pos = Helper.numeric_to_algebraic((x, y))
-                if move_dir == Helper.LINEAR_MOVES.DOWN:
-                    x, y = (x, y+1)
-                    next_pos = Helper.numeric_to_algebraic((x, y))
-                if move_dir == Helper.LINEAR_MOVES.LEFT:
-                    x, y = (x-1, y)
-                    next_pos = Helper.numeric_to_algebraic((x, y))
-                if move_dir == Helper.LINEAR_MOVES.RIGHT:
-                    x, y = (x+1, y)
-                    next_pos = Helper.numeric_to_algebraic((x, y))
+            x, y = Helper.get_coord_for_move(x, y, move_type, move_dir)
+            # print 'get_moves: x: {}, y: {}'.format(x, y)
+            next_pos = Helper.numeric_to_algebraic(x, y)
             if next_pos is not None and Helper.is_position_valid(next_pos):
                 distance -= 1
                 moves.append(next_pos)
             else:
                 distance = 0
         if len(moves) >= 1:
+            moves.sort()
             return moves
         return None
 
     @staticmethod
-    def get_linear_moves(x, y, distance, is_vertical=False):
-        start = x if not is_vertical else y
-        lower = 0 if distance is None or (start - distance) < 0 else (start - distance)
-        upper = 7 if distance is None or (start + distance) > 7 else (start + distance)
-        lower_moves = (start - lower)
-        upper_moves = (upper - start)
+    def get_coord_for_move(x, y, move_type, move_dir):
+        # print 'get_coord_for_move: x: {}, y: {}'.format(x, y)
+        if x is None or y is None or move_type is None or move_dir is None:
+            return None
+        coord = None
+        if move_type == Helper.MOVE_TYPE.DIAGONAL:
+            if move_dir == Helper.DIAGONAL_MOVES.SW:
+                coord = x - 1, y - 1
+            elif move_dir == Helper.DIAGONAL_MOVES.SE:
+                coord = x + 1, y - 1
+            elif move_dir == Helper.DIAGONAL_MOVES.NE:
+                coord = x + 1, y + 1
+            elif move_dir == Helper.DIAGONAL_MOVES.NW:
+                coord = x - 1, y + 1
+        elif move_type == Helper.MOVE_TYPE.LINEAR:
+            if move_dir == Helper.LINEAR_MOVES.DOWN:
+                coord = x, y - 1
+            elif move_dir == Helper.LINEAR_MOVES.UP:
+                coord = x, y + 1
+            elif move_dir == Helper.LINEAR_MOVES.LEFT:
+                coord = x - 1, y
+            elif move_dir == Helper.LINEAR_MOVES.RIGHT:
+                coord = x + 1, y
+        return coord
+
+    @staticmethod
+    def get_vertical_moves(pos, distance=None):
+        return Helper.get_linear_axis_moves(pos, distance, True)
+
+    @staticmethod
+    def get_horizontal_moves(pos, distance=None):
+        return Helper.get_linear_axis_moves(pos, distance, False)
+
+    @staticmethod
+    def get_linear_axis_moves(pos, distance=None, is_vertical=False):
+        if pos is None or not Helper.is_position_valid(pos):
+            return None
+        move_dirs = None
+        if not is_vertical:
+            move_dirs = [LinearMoves.LEFT, LinearMoves.RIGHT]
+        else:
+            move_dirs = [LinearMoves.UP, LinearMoves.DOWN]
+        return Helper.get_moves_for_dirs(pos, MoveType.LINEAR, move_dirs, distance)
+
+    @staticmethod
+    def get_all_moves(pos, distance=None):
+        if pos is None or not Helper.is_position_valid(pos):
+            return None
         moves = []
-        if left_moves > 0:
-            for i in range(1, down_moves):
-                print 'left: down: i: {}, moves: {}'.format(i, left_moves)
-                move = Helper.numeric_to_algebraic(x, (left + i))
-                moves.append(move)
-        if right_moves > 0:
-            for i in range(1, up_moves):
-                print 'right: up: i: {}, moves: {}'.format(i, right_moves)
-                move = Helper.numeric_to_algebraic(x, (right + i))
-                moves.append(move)
-        print 'moves: {}'.format(moves)
+        linear = Helper.get_linear_moves(pos, distance)
+        if linear is not None and len(linear) > 0:
+            moves.extend(linear)
+        diagonal = Helper.get_diagonal_moves(pos, distance)
+        if diagonal is not None and len(diagonal) > 0:
+            moves.extend(diagonal)
+        if len(moves) > 0:
+            return moves
+        return None
+
+
+    @staticmethod
+    def get_linear_moves(pos, distance=None):
+        if pos is None or not Helper.is_position_valid(pos):
+            return None
+        move_dirs = [LinearMoves.UP, LinearMoves.DOWN, LinearMoves.LEFT, LinearMoves.RIGHT]
+        return Helper.get_moves_for_dirs(pos, MoveType.LINEAR, move_dirs, distance)
+
+    @staticmethod
+    def get_diagonal_moves(pos, distance=None):
+        if pos is None or not Helper.is_position_valid(pos):
+            return None
+        move_dirs = [DiagonalMoves.NE, DiagonalMoves.NW, DiagonalMoves.SE, DiagonalMoves.SW]
+        return Helper.get_moves_for_dirs(pos, MoveType.DIAGONAL, move_dirs, distance)
+
+    @staticmethod
+    def get_moves_for_dirs(pos, move_type, move_dirs, distance=None):
+        if move_type is None or move_dirs is None or len(move_dirs) <= 0:
+            return None
+        moves = []
+        for move_dir in move_dirs:
+            result = Helper.get_moves(pos, move_type, move_dir, distance)
+            if result is not None and len(result) > 0:
+                moves.extend(result)
+        if moves is None or len(moves) <= 0:
+            return None
         return moves
 
 
@@ -161,7 +209,7 @@ class ChessPiece(object):
     prefix = ''
 
     def __init__(self, position):
-        if not self.is_legal_move(position):
+        if not Helper.is_position_valid(position):
             excep = '`{}` is not a legal start position'
             raise ValueError(excep.format(position))
         self.position = position
@@ -172,7 +220,7 @@ class ChessPiece(object):
         return Helper.algebraic_to_numeric(tile)
 
     def is_legal_move(self, position):
-        return True if Helper.is_position_valid(position) is not None else False
+        return Helper.is_position_valid(position)
 
     def move(self, position):
         if not self.is_legal_move(position):
@@ -181,3 +229,127 @@ class ChessPiece(object):
         self.moves.append(move)
         self.position = position
         return move
+
+
+class Rook(ChessPiece):
+    """A Rook piece on a chessboard.
+
+    Args:
+        position (string): The chess notation of the current position (eg,  'a8').
+
+    Attributes:
+       position (string): The position in chess notation.
+       moves (tuple): Move history of this piece.
+    """
+    prefix = 'R'
+
+    def __init__(self, position):
+        ChessPiece.__init__(self, position)
+        self.prefix = Rook.prefix
+
+    def is_legal_move(self, position):
+        if not Helper.is_position_valid(position):
+            return False
+        legal_moves = Helper.get_linear_moves(self.position)
+        if legal_moves is not None and position in legal_moves:
+            return True
+        return False
+
+
+class Bishop(ChessPiece):
+    """A Bishop piece on a chessboard.
+
+    Args:
+        position (string): The chess notation of the current position (eg,  'a8').
+
+    Attributes:
+       position (string): The position in chess notation.
+       moves (tuple): Move history of this piece.
+    """
+    prefix = 'B'
+
+    def __init__(self, position):
+        ChessPiece.__init__(self, position)
+        self.prefix = Bishop.prefix
+
+    def is_legal_move(self, position):
+        if not Helper.is_position_valid(position):
+            return False
+        legal_moves = Helper.get_diagonal_moves(self.position)
+        if legal_moves is not None and position in legal_moves:
+            return True
+        return False
+
+
+class King(ChessPiece):
+    """A King piece on a chessboard.
+
+    Args:
+        position (string): The chess notation of the current position (eg,  'a8').
+
+    Attributes:
+       position (string): The position in chess notation.
+       moves (tuple): Move history of this piece.
+    """
+    prefix = 'K'
+
+    def __init__(self, position):
+        ChessPiece.__init__(self, position)
+        self.prefix = King.prefix
+
+    def is_legal_move(self, position):
+        if not Helper.is_position_valid(position):
+            return False
+        legal_moves = Helper.get_all_moves(self.position, 1)
+        if legal_moves is not None and position in legal_moves:
+            return True
+        return False
+
+
+class ChessMatch(object):
+    """A Chess Match.
+
+    Args:
+        pieces (dict): The chess pieces keyed by full notation.
+
+    Attributes:
+       pieces (dict): The dict of pieces keyed by full notation.
+       log (tuple): Move history of this match.
+    """
+
+    def __init__(self, pieces=None):
+        self.pieces = None
+        self.log = []
+        if pieces is None:
+            self.reset()
+        else:
+            self.pieces = pieces
+
+    def reset(self):
+        self.pieces = {
+            'Ra1': Rook('a1'),
+            'Rh1': Rook('h1'),
+            'Ra8': Rook('a8'),
+            'Rh8': Rook('h8'),
+            'Bc1': Bishop('c1'),
+            'Bf1': Bishop('f1'),
+            'Bc8': Bishop('c8'),
+            'Bf8': Bishop('f8'),
+            'Ke1': King('e1'),
+            'Ke8': King('e8')
+        }
+        self.log = []
+
+    def move(self, piece_key, position):
+        piece = self.pieces.pop(piece_key)
+        if piece is None:
+            return
+        log_item = piece.move(position)
+        if not log_item:
+            return
+        self.log.append(log_item)
+        piece_key = '{}{}'.format(piece.prefix, piece.position)
+        self.pieces[piece_key] = piece
+
+    def __len__(self):
+        return len(self.log)
